bd4b5ecb3df264efa5f4736a9a0db54bf175bd9e
[Yashwanth Banda] <bandayashwanth@gmail.com>
2018-04-04 23:23:10 -0400

    Init Project

---
+++ src/main/java/com/spring4/practise/Spring4MVC/Spring4MvcApplication.java
@@ -0,0 +1,12 @@
+package com.spring4.practise.Spring4MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContoller.class, args);
+	}
+}
